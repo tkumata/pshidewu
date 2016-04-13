eb316dd1b8ea0c6956dfc2dd2550b3fe37778ec4
Tomokatsu Kumata <tkumata@gmail.com>
2016-04-13 13:49:46 +0900

    update 2016-04-13

--- pshidewu.ps1
+++ pshidewu.ps1
@@ -8,37 +8,35 @@
  # Require:
  #     PSWindowsUpdate module for PowerShell
  #
- # いらんこリスト last modify at 2016/03/02
+ # いらんこリスト last modify at 2016/04/13
 
-KB2952664 Win10アプグレ関連(Application Experience) (Win7用)
-KB2990214 Win10アプグレ関連(Win7用WindowsUpdateクライアント 詳細は ※備考 参照)
+KB2952664 Win10アプグレ関連(Application Experience) (Win7用) (注: KB2952664 は環境によっては中々消えない問題が発生しております。その際は何度も(数回～数十回)アンスコする必要有)
+KB2977759 CEIP関連
+KB2990214 Win10アプグレ関連(Win7用WindowsUpdateクライアント 詳細は ※備考1 参照)
 KB3021917 CEIP＆テレメトリ(遠隔情報収集)関連
-KB3035583 Win10アプグレ関連 1
-KB3035583 Win10アプグレ関連 2
-KB3050265 Win7用WindowsUpdateクライアント (Win10アプグレ関連と思われるファイル有)
-KB3065987 Win7用WindowsUpdateクライアント (同上)
+KB3022345 テレメトリ関連
+KB3035583 Win10アプグレ関連
 KB3068708 テレメトリ関連
 KB3075249 テレメトリ関連
-KB3075851 Win7用WindowsUpdateクライアント (同上)
 KB3080149 テレメトリ関連
+KB3123862 Win10アプグレ関連
+KB3050265 Win7用WindowsUpdateクライアント (Win10アプグレ関連と思われるファイル有)
+KB3065987 Win7用WindowsUpdateクライアント (同上)
+KB3075851 Win7用WindowsUpdateクライアント (同上)
 KB3083324 Win7用WindowsUpdateクライアント (同上)
 KB3083710 Win7用WindowsUpdateクライアント (同上)
 KB3102810 Win7用WindowsUpdateからWin10にアプグレ時の不具合の修正
 KB3112343 Win7用WindowsUpdateクライアント Win10アプグレ関連+KB3050265の不具合修正
-KB3123862 Win10アプグレ関連
 KB3135445 Win7用WindowsUpdateクライアント
 KB3138612 Win7用WindowsUpdateクライアント
 
-KB2977759 CEIP関連
-KB3022345 テレメトリ関連
-
-(注: KB2952664 は環境によっては中々消えない問題が発生しております。その際は何度も(数回～数十回)アンスコする必要有)
-
+2016/04/13 追記
+KB3148198 IE11 patch with Win10アプグレ関連
+KB2965664 Win10アプグレ関連
  #>
-
 param (
     [string]$check,
-    [string[]]$noWUKBs = @("KB2952664","KB2990214","KB3021917","KB3035583","KB3050265","KB3065987","KB3068708","KB3075249","KB3075851","KB3080149","KB3083324","KB3083710","KB3102810","KB3112343","KB3123862","KB3135445","KB3138612","KB2977759","KB3022345")
+    [string[]]$noWUKBs = @("KB2952664","KB2990214","KB3021917","KB3035583","KB3050265","KB3065987","KB3068708","KB3075249","KB3075851","KB3080149","KB3083324","KB3083710","KB3102810","KB3112343","KB3123862","KB3135445","KB3138612","KB2977759","KB3022345","KB3148198","KB2965664")
 )
 
 Import-Module PSWindowsUpdate
@@ -95,6 +93,7 @@ function toHidden() {
         Hide-WUUpdate -KBArticleID $findKBs -confirm:$false
     }
     echo "Searching finished."
+    echo "Please reboot Windows7."
 }
 
 function hiddenUpdatesList() {
